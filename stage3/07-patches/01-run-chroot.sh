334cf01ced7e443edc94b724989edfdb547130cc
Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>
2024-12-16 20:51:41 +0100

    Enable bluetooth service Enable NTP for timedatectl
    Signed-off-by: Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>

--- stage3/07-patches/01-run-chroot.sh
+++ stage3/07-patches/01-run-chroot.sh
@@ -7,8 +7,10 @@ chmod +x /etc/update-motd.d/*
 
 echo -e "\e[32m### Enabling services ###\e[0m"
 systemctl daemon-reload
-systemctl enable bettercap pwngrid-peer pwnagotchi
-systemctl disable wpa_supplicant apt-daily-upgrade.service apt-daily-upgrade.timer apt-daily.service apt-daily.timer bluetooth.service
+systemctl enable bettercap pwngrid-peer pwnagotchi bluetooth.service
+systemctl disable wpa_supplicant apt-daily-upgrade.service apt-daily-upgrade.timer apt-daily.service apt-daily.timer
+
+timedatectl set-ntp true
 
 echo -e "\e[32m### Disable apt packages from upgrading ###\e[0m"
 apt-mark hold firmware-atheros firmware-brcm80211 firmware-libertas firmware-misc-nonfree firmware-realtek libpcap-dev libpcap0.8 libpcap0.8-dev
