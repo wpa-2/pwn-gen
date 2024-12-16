9fe98f4d1c29afcc76e3612fea23d54008981b44
Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>
2024-12-16 21:06:51 +0100

    Revert
    Signed-off-by: Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>

--- stage3/07-patches/01-run-chroot.sh
+++ stage3/07-patches/01-run-chroot.sh
@@ -10,7 +10,5 @@ systemctl daemon-reload
 systemctl enable bettercap pwngrid-peer pwnagotchi bluetooth.service
 systemctl disable wpa_supplicant apt-daily-upgrade.service apt-daily-upgrade.timer apt-daily.service apt-daily.timer
 
-timedatectl set-ntp true
-
 echo -e "\e[32m### Disable apt packages from upgrading ###\e[0m"
 apt-mark hold firmware-atheros firmware-brcm80211 firmware-libertas firmware-misc-nonfree firmware-realtek libpcap-dev libpcap0.8 libpcap0.8-dev
