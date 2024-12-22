5939b3015b2a858644a9996171a4c4f97273263c
Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>
2024-12-22 11:52:21 +0100

    Update build
    Signed-off-by: Jeroen Oudshoorn <oudshoorn.jeroen@gmail.com>

--- stage3/01-pwn-packages/01-run.sh
+++ stage3/01-pwn-packages/01-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 echo -e "\e[32m### Creating /home/pi/handshakes ###\e[0m"
-install -v -d "${ROOTFS_DIR}/home/pi/handshakes"
+install -v -d -m 777 "${ROOTFS_DIR}/home/pi/handshakes"
