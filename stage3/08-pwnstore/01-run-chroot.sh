42dd1d9d12f162fc1db939ce348116a4891bf78a
wpa-2 <wpa3@protonmail.com>
2026-02-21 16:43:03 +0000

    Fix stage 08 verify script - remove pwnstore_ui check

--- stage3/08-pwnstore/01-run-chroot.sh
+++ stage3/08-pwnstore/01-run-chroot.sh
@@ -4,9 +4,4 @@ if [ ! -f /usr/bin/pwnstore ]; then
     echo -e "\e[31m[!] pwnstore CLI not found!\e[0m"
     exit 1
 fi
-echo -e "\e[32m### Verifying pwnstore_ui plugin ###\e[0m"
-if [ ! -f /usr/local/share/pwnagotchi/custom-plugins/pwnstore_ui.py ]; then
-    echo -e "\e[31m[!] pwnstore_ui plugin not found!\e[0m"
-    exit 1
-fi
 echo -e "\e[32m### PwnStore OK ###\e[0m"
